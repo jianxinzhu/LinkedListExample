922419bf81382c8900be973e8e6ae0e6026bfe21
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-03-20 01:09:25 -0400

    Create StackNode.hpp

---
+++ ReverseSentenceInStack/StackNode.hpp
@@ -0,0 +1,31 @@
+/*   name: jianxin.zhu
+ Complier: Visual Studio 2019
+     Time: 03/19/2020
+     
+*/
+
+
+#ifndef STACKNODE_H
+#define STACKNODE_H
+
+template<typename STACKNODE> class Stack;
+
+template<typename STACKNODE>
+class StackNode {
+	friend class Stack<STACKNODE>;
+
+public:
+	explicit StackNode(const STACKNODE& info)
+		:data(info), nextPtr(nullptr)
+	{
+
+	}
+	LISTNODE getData() const
+	{
+		return data;
+	}
+private:
+	STACKNODE data;
+	StackNode<STACKNODE>* nextPtr;
+};
+#endif
