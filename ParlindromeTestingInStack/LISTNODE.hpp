214fb9a6a6ca84c1447126eb6f487d6d35053678
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-03-31 22:38:29 -0400

    Create LISTNODE.hpp

---
+++ ParlindromeTestingInStack/LISTNODE.hpp
@@ -0,0 +1,37 @@
+/*   Name: jianxin.zhu
+  compier: Visual Studio 2019
+     date: 03/31/2020
+ */
+ 
+ 
+ #ifndef LISTNODE_H
+#define LISTNODE_H
+
+template<typename NODETYPE>
+class List;
+
+template<typename NODETYPE>
+class ListNode
+{
+	friend class List<NODETYPE>;
+
+public:
+	explicit ListNode(const NODETYPE& info)
+		:data(info), nextPtr(nullptr)
+	{
+
+    }
+
+	NODETYPE getData() const
+	{
+		return data;
+	}
+	ListNode<NODETYPE>* next() const
+	{
+		return nextPtr;
+	}
+private:
+	NODETYPE data;
+	ListNode<NODETYPE>* nextPtr;
+};
+#endif
