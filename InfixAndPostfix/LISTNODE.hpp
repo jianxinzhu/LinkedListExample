35e7054ec0a8eef39fae6a227b8731be533dfc67
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-04-01 02:07:20 -0400

    Create LISTNODE.hpp

---
+++ InfixAndPostfix/LISTNODE.hpp
@@ -0,0 +1,36 @@
+/*     name: jianxin zhu
+    Compiler: Visual studio 2019
+    date: 03/31/2020
+ */
+ 
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+template<typename NODETYPE>
+class List;
+
+template<typename NODETYPE>
+class ListNode
+{
+	friend class List<NODETYPE>;
+
+public:
+	explicit ListNode(const NODETYPE& info)
+		:data(info), nextPtr(nullptr)
+	{
+
+    }
+
+	NODETYPE getData() const
+	{
+		return data;
+	}
+	ListNode<NODETYPE>* next() const
+	{
+		return nextPtr;
+	}
+private:
+	NODETYPE data;
+	ListNode<NODETYPE>* nextPtr;
+};
+#endif
